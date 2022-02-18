88dfcbb97fc8fd321d70881857551a2623ca7c46
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-02-17 19:01:49 -0600

    integrated aws-sdk(SES) for sending emails, sending test email

--- routes/auth.js
+++ routes/auth.js
@@ -3,11 +3,19 @@ import { requireSignin } from "../middlewares";
 
 const router = expres.Router();
 
-const { register, login, logout, currentUser } = require("../controllers/auth");
+const {
+  register,
+  login,
+  logout,
+  currentUser,
+  testAWSEmail,
+} = require("../controllers/auth");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
 router.get("/current-user", requireSignin, currentUser);
+
+router.get("/send-email", testAWSEmail);
 
 module.exports = router;
