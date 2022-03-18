ae647f7030ffef52e0ec31f073d3ee7c22526af2
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-03-17 20:41:36 -0500

    implemented logic to create course validating if role=instructor

--- routes/course.js
+++ routes/course.js
@@ -1,9 +1,12 @@
-import expres from "express";
+import express from "express";
 // import { requireSignin } from "../middlewares";
-import { uploadImage, removeImage } from "../controllers/course";
-const router = expres.Router();
+import { uploadImage, removeImage, create } from "../controllers/course";
+import { isInstructor, requireSignin } from "../middlewares";
+const router = express.Router();
 
 router.post("/course/upload-image", uploadImage);
 router.post("/course/remove-image", removeImage);
+
+router.post("/course", requireSignin, isInstructor, create);
 
 module.exports = router;
