ae22f73e1b3c7dd15762f5fa21ad77b05bded454
Silvin Pradhan <silvinpradhan95@gmail.com>
2022-03-18 20:57:04 -0500

    implemented endpoint to get instructor courses

--- routes/instructor.js
+++ routes/instructor.js
@@ -7,11 +7,14 @@ const {
   makeInstructor,
   getAccountStatus,
   currentInstructor,
+  instructorCourses,
 } = require("../controllers/instructor");
 
 router.post("/make-instructor", requireSignin, makeInstructor);
 router.post("/get-account-status", requireSignin, getAccountStatus);
 
 router.get("/current-instructor", requireSignin, currentInstructor);
+
+router.get("/instructor-courses", requireSignin, instructorCourses);
 
 module.exports = router;
